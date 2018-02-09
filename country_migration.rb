2fcfc1485df4e6d4a5aa423d9dc428ebc87d2187
Cromon <CromonMS@users.noreply.github.com>
2018-02-09 15:18:33 +0000

    Update schema
    Update to include ISO Alpha 3 and ISO numeric codes
    http://www.nationsonline.org/oneworld/country_code_list.htm

--- country_migration.rb
+++ country_migration.rb
@@ -6,12 +6,16 @@ class CreateCountries < ActiveRecord::Migration
   def change
     create_table :countries do |t|
       t.string :name
-      t.string :iso_two_letter_code
+      t.string :iso_alpha_two
+      t.string :iso_alpha_three
+      t.string :iso_numeric
 
       t.timestamps null: false
 
       add_index(:countries, :name)
-      add_index(:countries, :iso_two_letter_code)
+      add_index(:countries, :iso_alpha_two)
+      add_index(:countries, :iso_alpha_three)
+      add_index(:countries, :iso_numeric)
     end
   end
 end
