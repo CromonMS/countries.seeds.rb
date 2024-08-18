62256c1ce991a64415b735b619cc9ce3e4b32828
Eugene <e@cromon.co.uk>
2024-08-18 15:41:59 +0100

    Add missing column for sovereignty, and enforce uniqueness on indexes, no 2 countries should share a name or iso attribute

--- country_migration.rb
+++ country_migration.rb
@@ -1,25 +1,26 @@
 # frozen_string_literal: true
 
-# rails g model Country name:string:index iso_alpha_two:string:index iso_alpha_three:string:index iso_numeric:integer:index
-
 # Create Countries migration
 class CreateCountries < ActiveRecord::Migration
   def change
     create_table :countries do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :official_name
       t.string :iso_alpha_two
       t.string :iso_alpha_three
       t.string :iso_numeric
       t.string :subdivision_codes
       t.string :tld
+      t.string :public_id
+      t.string :sovereignty
 
       t.timestamps null: false
     end
 
-    add_index :countries, :name
-    add_index :countries, :iso_alpha_two
-    add_index :countries, :iso_alpha_three
-    add_index :countries, :iso_numeric
+    add_index :countries, :name, unique: true
+    add_index :countries, :iso_alpha_two, unique: true
+    add_index :countries, :iso_alpha_three, unique: true
+    add_index :countries, :iso_numeric, unique: true
+    add_index :countries, :public_id, unique: true
   end
 end
